fc1f050e2d2d379e034a37f8e7177d9296bbb09d
Ilya Daraseliya <idarase@gmail.com>
2022-07-24 08:10:08 +0200

    dist to build

--- apps/worker-api/src/routes.ts
+++ apps/worker-api/src/routes.ts
@@ -1,26 +1,26 @@
-import { Engine } from './lib/exp/Engine'
-import { datafile } from '../datafile'
-import { storage } from './lib/exp/storage'
+import { Engine } from "./lib/exp/Engine";
+import { datafile } from "../datafile";
+import { storage } from "./lib/exp/storage";
 
 export const routes: Record<
   string,
   ((request: Request) => Promise<Response> | Response) | undefined
 > = {
-  '/api/experiment': async (request: Request) => {
-    const url = new URL(request.url)
-    const userId = url.searchParams.get('userId') || undefined
-    const countryCode = url.searchParams.get('countryCode') || undefined
+  "/api/experiment": async (request: Request) => {
+    const url = new URL(request.url);
+    const userId = url.searchParams.get("userId") || undefined;
+    const countryCode = url.searchParams.get("countryCode") || undefined;
     const engine = new Engine({
       datafile,
       ...(userId ? { storage: storage(userId) } : {}),
-    })
+    });
 
     const value = await engine.getVariationIds(userId, {
       countryCode,
-    })
-    return new Response(JSON.stringify(value) || null)
+    });
+    return new Response(JSON.stringify(value) || null);
   },
-  '/api/experiment/create': () => {
-    return new Response(null)
+  "/api/experiment/create": () => {
+    return new Response(null);
   },
-}
+};
