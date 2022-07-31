059a19cf58bf2631db8f22fdb175c14323dd2247
Ilya Daraseliya <idarase@gmail.com>
2022-07-31 17:31:25 +0200

    update read me

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,10 +1,7 @@
-import { Button } from "ui";
-
-export default function Docs() {
+export default function Index() {
   return (
     <div>
       <h1>Docs</h1>
-      <Button />
     </div>
   );
 }
